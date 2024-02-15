001676341571ef56246dcc30360e3c72e3464668
liyahayut <liyahayut12@gmail.com>
2024-02-15 19:57:36 +0200

    m

--- alice_room/alicemain.js
+++ alice_room/alicemain.js
@@ -2,3 +2,7 @@ function inpoo(){
     var div = document.getElementById("info");
     div.style.display = "none";
 }
+function inpoc(){
+    var div = document.getElementById("info");
+    div.style.display = "block";
+}
