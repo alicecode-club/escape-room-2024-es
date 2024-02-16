bb3fec519e272bf755f1c4f45a83acfa8d045490
liyahayut <liyahayut12@gmail.com>
2024-02-16 17:13:53 +0200

    k

--- alice_room/alicemain.js
+++ alice_room/alicemain.js
@@ -6,3 +6,7 @@ function inpoc(){
     var div = document.getElementById("info");
     div.style.display = "block";
 }
+function tunnel() {
+    var a=document.body.style.backgroundImage = "url('https://miro.medium.com/v2/resize:fit:1120/1*5QhRolKeDY9qTKSgIX5WUw.png')";
+    a.setAttribute("class", "back");
+}
