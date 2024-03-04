a7efe279e6e7271c3155f3c8f70c06f0edbbab86
liyahayut <liyahayut12@gmail.com>
2024-03-04 18:27:09 +0200

    l

---
+++ alice_room/alicesecond/alicesecond.js
@@ -0,0 +1,17 @@
+var audioa = new Audio('../sounda.mpeg');
+function audio(){
+    if (audioa && !audioa.paused) {
+        audioa.pause();
+    } else {
+        audioa = new Audio('../sounda.mpeg');
+        audioa.play();
+    }
+}
+function inpoc(){
+    var div = document.getElementById("info");
+    div.style.display = "none";
+}
+function inpoo(){
+    var div = document.getElementById("info");
+    div.style.display = "block";
+}
