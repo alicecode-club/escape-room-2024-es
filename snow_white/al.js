3a43da11f6c0f17cfbf838225c5ffc2440c6a591
shakedPasha <pashshaked@gmail.com>
2024-03-04 20:21:25 +0200

    snow

--- snow_white/al.js
+++ snow_white/al.js
@@ -1,3 +1,9 @@
 function lampPopup(){
-    alert("hello and welcome")
+    alert("hello and welcome to aladdin cave")
+    alert("Here you will collect the next ingredient for the evil queen's potion ")
+    alert("Just before you go searching you should know that the ingredient you are collecting is glowing and blue ")
+}
+
+function crystalPopup(){
+    alert("You picked up the first ingredient, good job")
 }
