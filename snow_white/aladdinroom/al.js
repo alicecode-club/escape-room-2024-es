e73f67f957d17e71367ea1d5eb87016db5f9cf4d
shakedPasha <pashshaked@gmail.com>
2024-03-07 19:29:02 +0200

    snow

--- snow_white/aladdinroom/al.js
+++ snow_white/aladdinroom/al.js
@@ -6,4 +6,15 @@ function lampPopup(){
 
 function crystalPopup(){
     alert("You picked up the first ingredient, good job")
+    alert("For the clue to the next location of the ingredients that you need to find, you need to find the gold face 💛")
+}
+
+function facePopup(){
+    alert("You found me!!")
+    alert("The next potion ingredient is in a muddy place with green creatures")
+    var answer = prompt('So where are we going to (which legan is this)?');
+    if (answer && answer.toLowerCase() === 'shrek') {
+        alert('Correct! shrek is the answer. You should remember the number 3');
+    } else {
+        alert("Wrong answer. try agian");}
 }
