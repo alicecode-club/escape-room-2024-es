46fcda94b3dd9c3eba2bfdee6ce12cfafb64f34c
shakedPasha <pashshaked@gmail.com>
2024-03-14 19:42:30 +0200

    snow

--- snow_white/aladdinroom/al.js
+++ snow_white/aladdinroom/al.js
@@ -1,16 +1,17 @@
 var lifes = 3;
 function lampPopup(){
+    localStorage.setItem("crystalCollcted", false);
     alert("hello and welcome to aladdin cave")
     alert("Here you will collect the next ingredient for the evil queen's potion ")
     alert("Just before you go searching you should know that the ingredient you are collecting is glowing and blue ")
 }
 
-var crystalCollcted=false
+console.log(localStorage.getItem("crystalCollcted"))
 function crystalPopup(){
     alert("You picked up the first ingredient, good job")
     alert("For the clue to the next location of the ingredients that you need to find, you need to find the gold face 💛")
-    crystalCollcted=true
-    console.log(crystalCollcted)
+    localStorage.setItem("crystalCollcted", true);
+    console.log(localStorage.getItem("crystalCollcted"))
 }
 
 function itemsup(){
@@ -22,7 +23,7 @@ function facePopup(){
     alert("The next potion ingredient is in a muddy place with green creatures")
     var answer = prompt('So where are we going to (which legan is this)?');
     if (answer && answer.toLowerCase() === 'shrek') {
-        alert('Correct! shrek is the answer. You should remember the number 3');
+        alert('Correct! shrek is the answer. You need to go to shrek swamp');
     } else {
         lifes -= 1;
         alert(`Wrong answer. You have another ${lifes} lives left.`);
--- snow_white/items/items.js
+++ snow_white/items/items.js
@@ -1,6 +1,7 @@
-var display=0
 document.getElementById('item1').style.display='none'
-if(crystalCollcted){
+console.log(!!localStorage.getItem("crystalCollcted"),typeof !!localStorage.getItem("crystalCollcted"))
+
+if(localStorage.getItem("crystalCollcted")==="true"){
     document.getElementById('item1').style.display='block'
     console.log("you have crystal")
 }
