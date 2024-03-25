36b266fd3f946a1ff6aa5b073b19d7404917787d
shakedPasha <pashshaked@gmail.com>
2024-03-25 10:13:18 +0200

    hii

--- snow_white/peterRoom/peter.js
+++ snow_white/peterRoom/peter.js
@@ -1,3 +1,4 @@
+var lifes = 3;
 localStorage.setItem("peterroom", true);
 document.getElementById ('correct').style.display ='none';
 document.getElementById ('incorrect').style.display ='none';
@@ -38,10 +39,41 @@ function HookPoPup(){
 }
 
 function mermaidPoPup(){
+    localStorage.setItem("mermaidSave", true);
     alert("You picked up one of the ingredients, good job")
     alert("To know where you need to go now you need to find:💀🪨")
     localStorage.setItem("waterCollcted", true);
+    document.getElementById('skull').style.display = 'inline-block';
 }
+
+function disableAllButtons() {
+    buttons.forEach(function(button) {
+        button.disabled = true;
+    });
+}
+
+function skullPoPup(){
+    localStorage.setItem("skullSave", true);
+    alert("Welcome to the skull rock and the last clue that will lead you to escape from neverland")
+    alert("The place where you will collect the last clue is a muddy place inhabited by green creatures")
+    var answer3 = prompt('So where are we going to (which legan is this)?');
+    if (answer3 && answer3.toLowerCase() === 'sherk') {
+        alert('Correct! sherk is the answer. You need to go to to the swamp of sherk');
+        window.location.href="../shrek_room/shreck_room.html"
+        
+    } else {
+        lifes -= 1;
+        alert(`Wrong answer. You have another ${lifes} lives left.`);
+
+        if (lifes === 0) {
+            alert("You failed the game 😭");
+            disableAllButtons()
+        } else {
+            skullPoPup();
+    }
+}}
+
+
 
 if(localStorage.getItem("hookShip")==="true"){
     document.getElementById ('Hook').style.display ='none';
@@ -49,6 +81,9 @@ if(localStorage.getItem("hookShip")==="true"){
 }
 
 if(localStorage.getItem("jollyPoup")==="true"){
-    document.getElementById ('Hook').style.display ='none';
     document.getElementById('mermaid').style.display = 'inline-block';    
+}
+
+if(localStorage.getItem("mermaidSave")==="true"){
+    document.getElementById('skull').style.display = 'inline-block';    
 }
